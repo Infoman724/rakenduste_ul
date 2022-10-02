685c86ed4140ba6924b016a4e3c23b4928f7cec9
official <official@DESKTOP-67HJKDE>
2022-10-02 18:37:02 +0200

    работает второе задание но нужно поменять имена на эст

--- rakenduste_ul/Program.cs
+++ rakenduste_ul/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using rakenduste_ul;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,17 +7,17 @@ using System.Windows.Forms;
 
 namespace rakenduste_ul
 {
-    internal static class Program
+    static class Program
     {
         /// <summary>
-        /// Главная точка входа для приложения.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
